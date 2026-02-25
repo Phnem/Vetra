c9905238d02fa8f02aadc70561cd689ed47c598c
Phnem <phnem@users.noreply.github.com>
2026-02-25 22:04:37 +0200

    huge update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "v3.0.3-Beta"
+        versionName = "v3.0.4-Stable"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         buildConfigField("String", "GITHUB_OWNER", "\"Phnem\"")
